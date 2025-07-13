8c1e60e5051a52c947ee75124bed52555891bf36
Shikhar Verma <shikhar1verma@gmail.com>
2025-07-13 16:37:17 +0530

    enhancement: Restructuring frontend folder.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
-import FileUploader from '../components/FileUploader';
-import ChatWindow from '../components/ChatWindow';
+import FileUploader from '@/components/FileUploader';
+import ChatWindow from '@/components/ChatWindow';
 
 export default function Home() {
   return (
